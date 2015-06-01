bee55a17210ca194da3b518f8149b5a1a1cd2cea
wlMalk <wm.wexem@gmail.com>
2015-06-01 05:45:52 +0300

    Getter functions for Route

--- route.go
+++ route.go
@@ -17,6 +17,27 @@ func NewRoute(path string) *Route {
 	}
 }
 
+// GetParent returns the parent of the route.
+func (r *Route) GetParent() *Route {
+	return r.parent
+}
+
+// GetName returns the description of the route.
+func (r *Route) GetDescription() string {
+	return r.description
+}
+
+// GetPath returns the relative path of the route to the
+// parent route.
+func (r *Route) GetPath() string {
+	return r.path
+}
+
+// GetRoutes returns the map of child routes.
+func (r *Route) GetRoutes() map[string]*Route {
+	return r.routes
+}
+
 // GetSchemes returns the default schemes passed from
 // the parent.
 func (r *Route) GetSchemes() []string {
