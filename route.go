51bc3b6e4c44a582ee9a74a3540bad698cee2bde
wlMalk <wm.wexem@gmail.com>
2015-06-01 05:48:24 +0300

    More setters and getters for Route

--- route.go
+++ route.go
@@ -22,14 +22,40 @@ func (r *Route) GetParent() *Route {
 	return r.parent
 }
 
+// Name sets the name of the route.
+// The route name is only used for documentation purposes.
+func (r *Route) Name(name string) *Route {
+	r.name = name
+	return r
+}
+
+// GetName returns the name of the route.
+func (r *Route) GetName() string {
+	return r.name
+}
+
 // GetName returns the description of the route.
 func (r *Route) GetDescription() string {
 	return r.description
 }
 
+// Description sets the description of the route.
+func (r *Route) Description(description string) *Route {
+	r.description = description
+	return r
+}
+
 // GetPath returns the relative path of the route to the
 // parent route.
 func (r *Route) GetPath() string {
+	return r.path
+}
+
+// GetFullPath returns the absolute path of the route.
+func (r *Route) GetFullPath() string {
+	if r.parent != nil {
+		return r.parent.GetFullPath() + r.path
+	}
 	return r.path
 }
 
