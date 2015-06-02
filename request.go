1b83ae6ac072ddc951f16f8985013fde8ac73578
wlMalk <wm.wexem@gmail.com>
2015-06-02 07:36:20 +0300

    Updating Request and Response structs

--- request.go
+++ request.go
@@ -1,4 +1,19 @@
 package ergo
 
+import (
+	"net/http"
+)
+
 type Request struct {
+	*http.Request
+	Input      map[string]validation.Valuer
+	pathParams map[string]string
+	route      *Route // route object that matched request
+}
+
+func NewRequest(httpRequest *http.Request) *Request {
+	return &Request{
+		Request: httpRequest,
+		Input:   map[string]validation.Valuer{},
+	}
 }
--- response.go
+++ response.go
@@ -1,4 +1,19 @@
 package ergo
 
+import (
+	"net/http"
+)
+
 type Response struct {
+	http.ResponseWriter
+	statusCode    int
+	contentLength int
+	indent        bool
+}
+
+func NewResponse(httpResponse http.ResponseWriter) *Response {
+	return &Response{
+		ResponseWriter: httpResponse,
+		statusCode:     http.StatusOK,
+	}
 }
