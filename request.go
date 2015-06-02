7f08592da6b612bf33dda64a404e7f44c5c3cfb8
wlMalk <wm.wexem@gmail.com>
2015-06-02 07:57:34 +0300

    Implement validation.Requester interface for Request

--- request.go
+++ request.go
@@ -2,6 +2,8 @@ package ergo
 
 import (
 	"net/http"
+
+	"github.com/wlMalk/ergo/validation"
 )
 
 type Request struct {
@@ -16,4 +18,37 @@ func NewRequest(httpRequest *http.Request) *Request {
 		Request: httpRequest,
 		Input:   map[string]validation.Valuer{},
 	}
+}
+
+// Req returns the request.
+func (r *Request) Req() *http.Request {
+	return r.Request
+}
+
+// Param returns the input parameter value by its name.
+func (r *Request) Param(name string) validation.Valuer {
+	return r.Input[name]
+}
+
+// ParamOk returns the input parameter value by its name.
+func (r *Request) ParamOk(name string) (validation.Valuer, bool) {
+	p, ok := r.Input[name]
+	return p, ok
+}
+
+// Params returns a map of input parameters values by their names.
+// If no names given then it returns r.Input
+func (r *Request) Params(names ...string) map[string]validation.Valuer {
+	if len(names) == 0 {
+		return r.Input
+	}
+	params := map[string]validation.Valuer{}
+	for _, n := range names {
+		p, ok := r.Input[n]
+		if !ok {
+			continue
+		}
+		params[n] = p
+	}
+	return params
 }
