223927c13b9ccdbd8cdf7bfea5d5e77c2e9e7521
wlMalk <wm.wexem@gmail.com>
2015-06-07 05:09:48 +0300

    Move constants.go to its own package

---
+++ constants/constants.go
@@ -0,0 +1,28 @@
+package constants
+
+const (
+	PARAM_STRING = iota
+	PARAM_INT
+	PARAM_INT64
+	PARAM_FLOAT
+	PARAM_FLOAT64
+	PARAM_BOOL
+
+	IN_PATH = iota
+	IN_QUERY
+	IN_HEADER
+	IN_BODY
+
+	METHOD_ANY    = ""
+	METHOD_GET    = "GET"
+	METHOD_POST   = "POST"
+	METHOD_PUT    = "PUT"
+	METHOD_DELETE = "DELETE"
+
+	SCHEME_HTTP  = "http"
+	SCHEME_HTTPS = "https"
+
+	MIME_XML  = "application/xml"
+	MIME_JSON = "application/json"
+
+)
