f220766eb8346d0ba4917d953257a2c9d1e5071e
wlMalk <wm.wexem@gmail.com>
2015-06-07 05:16:28 +0300

    Changes to Ergo functions

--- ergo.go
+++ ergo.go
@@ -34,6 +34,9 @@ func New() *Ergo {
 	e.root = r
 	return e
 }
+
+func (e *Ergo) New(path string) *Route {
+	return e.root.New(path)
 }
 
 func (e *Ergo) Schemes(s ...string) *Ergo {
@@ -52,37 +55,78 @@ func (e *Ergo) Produces(mimes ...string) *Ergo {
 }
 
 func (e *Ergo) Params(params ...*Param) *Ergo {
-	addParams(e, params...)
+	addParams(e.root, params...)
 	return e
 }
 
 func (e *Ergo) ResetParams(params ...*Param) *Ergo {
-	e.setParamsSlice(params...)
+	e.root.setParamsSlice(params...)
 	return e
 }
 
 func (e *Ergo) SetParams(params map[string]*Param) *Ergo {
-	e.setParams(params)
+	e.root.setParams(params)
 	return e
 }
 
 func (e *Ergo) IgnoreParams(params ...string) *Ergo {
-	ignoreParams(e, params...)
+	ignoreParams(e.root, params...)
 	return e
 }
 
 func (e *Ergo) IgnoreParamsBut(params ...string) *Ergo {
-	ignoreParamsBut(e, params...)
+	ignoreParamsBut(e.root, params...)
 	return e
 }
 
-func (e *Ergo) NotFoundHandler(h Handler) *Ergo {
-	e.Route.notFoundHandler = h
-	return e
+// Router uses a router that implement Router interface
+// as the main router.
+func (e *Ergo) Router(er ExternalRouter) {
+
+}
+
+// GetSchemes returns the default schemes.
+func (e *Ergo) GetSchemes() []string {
+	return e.schemes
+}
+
+// GetConsumes returns the default consumable content types.
+func (e *Ergo) GetConsumes() []string {
+	return e.consumes
+}
+
+// GetProduces returns the default producible content types.
+func (e *Ergo) GetProduces() []string {
+	return e.produces
+}
+
+func (e *Ergo) setSchemes(schemes []string) {
+	e.schemes = schemes
+}
+
+func (e *Ergo) setConsumes(consumes []string) {
+	e.consumes = consumes
+}
+
+func (e *Ergo) setProduces(produces []string) {
+	e.produces = produces
 }
 
 func (e *Ergo) Prepare() error {
 	return nil
+}
+
+func (e *Ergo) NotFound(res *Response, req *Request) {
+	e.NotFoundHandler.ServeHTTP(res, req)
+}
+func (e *Ergo) MethodNotAllowed(r *Route, res *Response, req *Request) {
+	e.MethodNotAllowedHandler.ServeHTTP(r, res, req)
+}
+func (e *Ergo) Err(err error, res *Response, req *Request) {
+	e.ErrHandler.ServeHTTP(err, res, req)
+}
+func (e *Ergo) Panic(res *Response, req *Request) {
+	e.PanicHandler.ServeHTTP(res, req)
 }
 
 func (e *Ergo) Run(address string) error {
@@ -108,14 +152,13 @@ func (e *Ergo) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, r.URL.String(), http.StatusMovedPermanently)
 		return
 	}
-	route, rp := e.Match(path)
+	route, rp := e.root.Match(path)
 	if route == nil {
 		// not found
 		return
 	}
 
 	req := NewRequest(r)
-	req.route = route
 	if len(rp) > 0 {
 		ps := strings.Split(rp[:len(rp)], ";")
 		for _, p := range ps {
